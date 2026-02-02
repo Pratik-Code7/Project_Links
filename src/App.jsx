664658edaa79ea3613bfc373533da86235396840
Pratik-Code7 <shakyapratik4@gmail.com>
2026-02-02 18:48:03 +0545

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -17,22 +17,82 @@ import story from "./assets/story.png";
 import theme from "./assets/theme.png";
 const App = () => {
   const data = [
-    { title: "Potfolio Website", link: potfoliob },
-    { title: "Task Manager", link: taskmanager },
-    { title: "Rock Paper ", link: rock },
-    { title: "Tic Tac Toe", link: tic },
-    { title: "Amazon", link: amazon },
-    { title: "Potfolio", link: potfolio },
-    { title: "Friend Request", link: friendrequest },
-    { title: "To do List", link: todo },
-    { title: "Insta Like", link: instalike },
-    { title: "Currency", link: currency },
-    { title: "Story", link: story },
-    { title: "Quote", link: quote },
-    { title: "Num Guess", link: numguess },
-    { title: "Theme", link: theme },
-    { title: "Quiz", link: quiz },
-    { title: "Calculator", link: Calculator },
+    {
+      title: "Potfolio Website",
+      link: potfoliob,
+      live: "https://pratik-code7.github.io/Potfolio_website/",
+    },
+    {
+      title: "Task Manager",
+      link: taskmanager,
+      live: "https://pratik-code7.github.io/React-Task-Manager/",
+    },
+    {
+      title: "Rock Paper Scissors",
+      link: rock,
+      live: "https://pratik-code7.github.io/Rock-Paper-Scissors/",
+    },
+    {
+      title: "Tic Tac Toe",
+      link: tic,
+      live: "https://pratik-code7.github.io/Tic-Tac-Toe/",
+    },
+    {
+      title: "Amazon",
+      link: amazon,
+      live: "https://pratik-code7.github.io/Amazon-Clone/Amazon/",
+    },
+    {
+      title: "Potfolio",
+      link: potfolio,
+      live: "https://pratik-code7.github.io/Potfolio/My potfolio",
+    },
+    {
+      title: "Friend Request",
+      link: friendrequest,
+      live: "https://pratik-code7.github.io/Friend-Request/",
+    },
+    {
+      title: "To do List",
+      link: todo,
+      live: "https://pratik-code7.github.io/To-Do-List/",
+    },
+    {
+      title: "Insta Like",
+      link: instalike,
+      live: "https://pratik-code7.github.io/Insta-Like/",
+    },
+    {
+      title: "Currency",
+      link: currency,
+      live: "https://pratik-code7.github.io/Currency-Converter/",
+    },
+    {
+      title: "Story",
+      link: story,
+      live: "https://pratik-code7.github.io/Insta-Story/",
+    },
+    {
+      title: "Quote",
+      link: quote,
+      live: "https://pratik-code7.github.io/Quote/",
+    },
+    {
+      title: "Num Guess",
+      link: numguess,
+      live: "https://pratik-code7.github.io/Number-Guessing-Game/",
+    },
+    {
+      title: "Theme",
+      link: theme,
+      live: "https://pratik-code7.github.io/Theme-toggle/",
+    },
+    { title: "Quiz", link: quiz, live: "https://pratik-code7.github.io/Quiz/" },
+    {
+      title: "Calculator",
+      link: Calculator,
+      live: "https://pratik-code7.github.io/Calculator-Project/",
+    },
   ];
   return (
     <div className="p-5 w-screen h-screen overflow-x-auto">
@@ -41,7 +101,7 @@ const App = () => {
       </h1>
       <div className="flex flex-wrap justify-center gap-10 h-full w-full">
         {data.map((elem, idx) => {
-          return <Card title={elem.title} link={elem.link} />;
+          return <Card title={elem.title} link={elem.link} live={elem.live} />;
         })}
       </div>
     </div>
